d5655a06e8cda6b155ed04551597b91d9940232f
kirillmc <kirillmcdev@gmail.com>
2024-02-22 15:13:08 +0300

    auth was created acording to exercise for week_01

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,11 +1,63 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	desc "github.com/kirillmc/auth/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
+
+	"log"
+	"net"
+)
+
 const grpcPort = 50051
 
 type server struct {
-	//desc.Unimple
+	desc.UnimplementedUserV1Server
+}
+
+// Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateResponse, error)
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	return &desc.CreateResponse{
+		Id: 101,
+	}, nil
+}
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      "AAA",
+		Email:     "AAA",
+		Role:      desc.Role_user,
+		CreatedAt: timestamppb.New(time.Now()),
+		UpdatedAt: timestamppb.New(time.Now()),
+	}, nil
+}
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("id of updated user: %d", req.GetId())
+	return nil, nil
+}
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("id of deleted user: %d", req.GetId())
+	return nil, nil
 }
 
 func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	desc.RegisterUserV1Server(s, &server{})
+	log.Printf("server is listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,17 @@ module github.com/kirillmc/auth
 go 1.21
 
 require (
-	github.com/fatih/color v1.15.0 // indirect
+	github.com/fatih/color v1.15.0
+	google.golang.org/grpc v1.62.0
+	google.golang.org/protobuf v1.32.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
 )
